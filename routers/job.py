759c1c75f4573345a074c08a4a15c4c2ff2cdaea
preety-m <preetydevim2004@gmail.com>
2026-06-26 16:38:37 +0530

    fastapi schemas

--- routers/job.py
+++ routers/job.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from schema.job import JobCreate,JobUpdate
 router = APIRouter(prefix="/job", tags={"job"})
+job = []
 
 @router.get("/")
 def read_job():
@@ -8,3 +10,12 @@ def read_job():
 @router.get("/{job_id}")
 def read_job_by_id(job_id: int):
     return {"job_id": job_id}   
+
+@router.put("/{job_id}")
+def update_job(job_id:int,job:JobUpdate):
+    job[job_id]=job
+    return job
+@router.delete("/{job_id}")
+def delete_job(job_id:int):
+    job.pop(job_id)
+    return job
---
+++ schema/company.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class CompanyCreate(BaseModel):
+    name: str
+    location: str
+    
+class CompanyUpdate(BaseModel):
+    name: Optional[str] = None
+    location: Optional[str] = None
+
+   
