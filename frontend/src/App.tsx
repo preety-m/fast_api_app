71215e508d64364c5c9ce7b0e09d0bb1c08a0b2e
preety-m <preetydevim2004@gmail.com>
2026-06-29 16:44:11 +0530

    frontend components updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import Welcome from "./components/welcome";
+import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
+import Footer from "./components/Footer";
 function App(){
   return(
     <>
     <NavBar/>
     <Welcome/>
+    <CompanyCard/>
+    <JobCard/>
+    <Footer/>
     </>
-    
   )
 }
 export default App
---
+++ frontend/src/components/JobCard.tsx
@@ -0,0 +1,11 @@
+function JobCard(){
+    return(
+        <div>
+            <h2>Software Engineer</h2>
+            <p>Google</p>
+            <p>Bangalore</p>
+            <p>5 LPA</p>
+        </div>
+    )
+}
+export default JobCard
