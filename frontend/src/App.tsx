f554404234ac9ce47675ad405ce627395250df32
preety-m <preetydevim2004@gmail.com>
2026-06-30 16:27:21 +0530

    frontend and backend integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,54 @@
+
 import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
 import Footer from "./components/Footer";
-function App(){
-  return(
-    <>
-    <NavBar/>
-    <Welcome/>
-    <CompanyCard/>
-    <JobCard/>
-    <Footer/>
-    </>
-  )
+import { useEffect, useState } from "react";
+import { getCompanies } from "./Services/CompanyService";
+import type { Company } from "./types/company";
+
+function App() {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
+    const [companies, setCompanies] = useState<Company[]>([]);
+
+    async function fetchCompanies() {
+        setLoading(true);
+        try {
+          const companies = await getCompanies();
+          setCompanies(companies);
+        }
+        catch (error) {
+          setError(error as Error);
+        }
+        finally {
+          setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchCompanies();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <>
+        <NavBar/>
+        <Welcome/>
+        <br/>
+        <CompanyCard companies={companies}/>
+        <JobCard/>
+        <Footer/> 
+        </>
+    )
 }
+   
 export default App
