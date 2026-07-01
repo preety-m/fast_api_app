3bb92641450cd17b7c7579a1fe430f9c1adb82da
preety-m <preetydevim2004@gmail.com>
2026-07-01 13:58:21 +0530

    Restructure project into backend and frontend

---
+++ backend/models/users.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column,Integer,String
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    hashed_password = Column(String(255), nullable=False)
+    role=Column(String(255),nullable=False)
+    
